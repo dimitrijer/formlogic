de60cd36e3f128b633756b8da8d894ed16d20860
Dimitrije Radojević <dimitrijer@nordeus.eu>
2016-09-10 14:24:36 +0200

    Add Ring+Compojure deps

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ; Logging dependencies.
                  [org.clojure/tools.logging "0.3.1"]
@@ -11,7 +12,14 @@
                  [org.apache.logging.log4j/log4j-api "2.6.2"]
                  [org.apache.logging.log4j/log4j-core "2.6.2"]
                  ; Configuration library.
-                 [levand/immuconf "0.1.0"]]
+                 [levand/immuconf "0.1.0"]
+                 ; Ring + Compojure
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
   :main ^:skip-aot formlogic.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler formlogic.handler/app}
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]]}})
