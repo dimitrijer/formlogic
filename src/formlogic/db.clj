21a6f3c17017563e1956d2d2213100c9496564cc
Dimitrije Radojević <dimitrijer@nordeus.eu>
2016-09-17 00:39:35 +0200

    Use DB connection configuration from config.edn

--- src/formlogic/db.clj
+++ src/formlogic/db.clj
@@ -1,10 +1,14 @@
 (ns formlogic.db
-  (:require [yesql.core :refer [defqueries]]))
+  (:require [yesql.core :refer [defqueries]]
+            [formlogic.config :refer [cget]]))
 
 (def db-spec {:classname "org.postgresql.Driver"
               :subprotocol "postgresql"
-              :subname "//localhost:5432/formlogic"
-              :user "formlogic"})
+              :subname (format "//%s:%d/%s"
+                               (cget :db :host)
+                               (cget :db :port)
+                               (cget :db :database))
+              :user (cget :db :user)})
 
 (defqueries "sql/get.sql" {:connection db-spec})
 (defqueries "sql/insert.sql" {:connection db-spec})
