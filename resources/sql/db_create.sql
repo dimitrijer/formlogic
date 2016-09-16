25ff4dca2034c4a99e8a36fc3bb4d1aafeae54e0
Dimitrije Radojević <dimitrijer@nordeus.eu>
2016-09-16 20:52:24 +0200

    Add more contents to db_create.sql

--- resources/sql/db_create.sql
+++ resources/sql/db_create.sql
@@ -1,8 +1,61 @@
 -- User table.
-CREATE TABLE public.user(
+CREATE TABLE public.user (
 	id SERIAL NOT NULL,
 	email text NOT NULL,
 	password text NOT NULL,
 	admin boolean NOT NULL DEFAULT false,
 	PRIMARY KEY("id")
+);
+
+CREATE UNIQUE INDEX user_email_idx ON "user" ("email");
+
+-- Score for user and assignment.
+CREATE TABLE public.user_score (
+	id SERIAL NOT NULL,
+	user_id int4 NOT NULL,
+	assignment_id int4 NOT NULL,
+	started_at timestamptz NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
+	completed_at timestamptz,
+	score int4 NOT NULL DEFAULT 0,
+	PRIMARY KEY("id")
+);
+
+ALTER TABLE "user_score" ADD CONSTRAINT "Ref_user_score_to_user"
+	FOREIGN KEY ("user_id") REFERENCES "user"("id")
+	MATCH SIMPLE
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+-- ###########
+-- STATIC DATA
+-- ###########
+CREATE SCHEMA static;
+
+-- Assignments.
+CREATE TABLE static.assignment (
+	id SERIAL NOT NULL,
+	name text NOT NULL,
+	PRIMARY KEY("id")
+);
+
+-- Tasks specific to assignments.
+CREATE TABLE static.task (
+	id int4 NOT NULL,
+	assignment_id int4 NOT NULL,
+	contents text NOT NULL,
+	PRIMARY KEY("id", "assignment_id")
+);
+
+-- Types of questions.
+CREATE TYPE static.question_type AS ENUM ('fill', 'single', 'multiple');
+
+-- Questions specific to a task.
+CREATE TABLE static.question (
+	id int4 NOT NULL,
+	task_id int4 NOT NULL,
+	type static.question_type NOT NULL,
+	body text NOT NULL,
+	answer text NOT NULL,
+	PRIMARY KEY ("id", "task_id")
 );
