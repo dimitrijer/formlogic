aef58da7a5ab6df2b974d9653d03b0426c21dff8
Dimitrije Radojević <dimitrijer@nordeus.eu>
2016-09-28 01:23:15 +0200

    WIP on docs (3rd chapter)

--- resources/sql/assignments.sql
+++ resources/sql/assignments.sql
@@ -33,6 +33,30 @@ VALUES
 INSERT INTO static.assignment(id, name, category)
 VALUES (3, 'Opšta kultura 3 - Nauka', 'Opšte znanje');
 
+INSERT INTO static.task(assignment_id, ord)
+VALUES (3, 1);
+
+INSERT INTO static.question(task_id, ord, type, body, choices, answers)
+VALUES
+(4, 1, 'single', 'Koje godine je čovek prvi put sleteo na Mesec?', '{"1989", "1975", "1969", "1953"}', '{2}'::int2[]);
+
+INSERT INTO static.task(assignment_id, ord, contents)
+VALUES (3, 2, '[:img {:style "max-width: 100%;" :src "https://ned.ipac.caltech.edu/level5/Sept05/Gawiser2/Figures/figure3.jpg"}]');
+
+INSERT INTO static.question(task_id, ord, type, body, choices, answers)
+VALUES
+(5, 2, 'single', 'Koji fizičar je otkrio zakon gravitacije?', '{"Isak Njutn", "Johanes Kepler", "Tiho Brahe"}', '{0}'::int2[]),
+(5, 3, 'multiple', 'Za koja od ponuđenih naučnih dostignuća je dodeljena Nobelova nagrada?', '{"Radijacija", "Mikrokosmičko pozadinsko zračenje", "Antibiotici", "Heliocentični sistem", "Struktura atoma"}', '{0, 1, 2, 4}'::int2[]),
+(5, 4, 'fill', 'Brzina svetlosti u kilometrima po sekundi iznosi:', '{"300000"}', '{0}'::int2[]);
+
+
+INSERT INTO static.task(assignment_id, ord)
+VALUES (3, 3);
+
+INSERT INTO static.question(task_id, ord, type, body, choices, answers)
+VALUES
+(6, 5, 'fill', 'Koliko elemenata iz periodnog sistema se mogu naći u prirodi?', '{"94"}', '{0}'::int2[]);
+
 INSERT INTO static.assignment(id, name, category)
 VALUES (4, 'Formalna logika', 'Ekspertski sistemi');
 
