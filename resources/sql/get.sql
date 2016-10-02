96360c1cee40091b6818fdd68cb66646d247f9f2
Dimitrije Radojević <dimitrijer@nordeus.eu>
2016-10-02 07:20:08 +0200

    Fix query for completed progresses if user has 0 score

--- resources/sql/get.sql
+++ resources/sql/get.sql
@@ -92,10 +92,9 @@ questions AS (SELECT p.id, correct, count(*) AS cnt FROM question_progress qp
               INNER JOIN progress p ON qp.assignment_progress_id = p.id
               GROUP BY p.id, qp.correct),
 total_questions AS (SELECT id, sum(cnt) AS total FROM questions GROUP BY id)
-SELECT p.id, p.started_at, p.completed_at, u.email, (100 * cnt::real / total::real)::int AS grade
+SELECT p.id, p.started_at, p.completed_at, u.email, (100 * COALESCE(cnt::real, 0::real) / total::real)::int AS grade
 FROM progress p
 INNER JOIN public.user u ON p.user_id = u.id
-INNER JOIN questions q ON p.id = q.id
+LEFT OUTER JOIN questions q ON p.id = q.id AND q.correct
 INNER JOIN total_questions tq ON p.id = tq.id
-WHERE q.correct
 ORDER BY p.completed_at DESC;
